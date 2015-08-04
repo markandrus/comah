c590aac5771721dba9f38b1c35be0f5fe4479bcf
Mark Andrus Roberts <markandrusroberts@gmail.com>
2015-08-04 00:32:46 -0700

    Add some Fantasy Land checks

--- test.js
+++ test.js
@@ -1,61 +1,65 @@
 'use strict';
+var array = require('./lib/array');
+var Node = require('./lib/node');
 
-var assert = require('assert');
+var λ = require('fantasy-check/src/adapters/mocha');
+var semigroup = require('fantasy-check/src/laws/semigroup');
+var functor = require('fantasy-check/src/laws/functor');
+var applicative = require('fantasy-check/src/laws/applicative');
+var monad = require('fantasy-check/src/laws/monad');
+var comonad = require('fantasy-check/src/laws/comonad');
 
-describe('Foo', function() {
+function run(node) {
+  return node.reduce(function(as, a) {
+    as.push(a);
+    return as;
+  }, []);
+}
 
-  // console.log('Building "Foo"');
+// Create a rose tree with a somewhat interesting shape.
+function nodeOfArray(a) {
+  return Node.of([a])
+    .insertLeft(
+      Node.of([a])
+        .insertLeft(Node.of([a])))
+    .insertRight([a]);
+}
 
-  beforeEach(function() {
-    // console.log('Before "bar" and "qux"');
+function runNodeOfArray(node) {
+  return array.join(run(node));
+}
+
+describe('Node', function() {
+  describe('Semigroup', function() {
+    it('all', semigroup.laws(λ)(nodeOfArray, runNodeOfArray));
+    it('associativity', semigroup.associativity(λ)(nodeOfArray, runNodeOfArray));
   });
 
-  it('bar', function() {
-    // Pass
+  describe('Functor', function() {
+    it('all', functor.laws(λ)(nodeOfArray, runNodeOfArray));
+    it('identity', functor.identity(λ)(nodeOfArray, runNodeOfArray));
+    it('composition', functor.composition(λ)(nodeOfArray, runNodeOfArray));
   });
 
-  describe('Baz', function() {
-
-    // console.log('Building "Baz"');
-
-    beforeEach(function() {
-      // console.log('Before "qux"');
-    });
-
-    it('qux', function() {
-      // Pass
-    });
-
-    it('quux', function() {
-      assert(false);
-    });
-
-    afterEach(function() {
-      // console.log('After "qux"');
-    });
-
+  describe('Applicative', function() {
+    it('all', applicative.laws(λ)(Node, run));
+    it('identity',  applicative.identity(λ)(Node, run));
+    it('composition', applicative.composition(λ)(Node, run));
+    it('homomorphism', applicative.homomorphism(λ)(Node, run));
+    it('interchange', applicative.interchange(λ)(Node, run));
   });
 
-  describe('Corge', function() {
-
-    // console.log('Building "Corge"');
-
-    beforeEach(function(done) {
-      setTimeout(done, 1000);
-    });
-
-    it('grault', function(done) {
-      setTimeout(done.bind(null, new Error('Oops')), 1000);
-    });
-
-    it('garply', function(done) {
-      setTimeout(done, 2000);
-    });
-
+  describe('Monad', function() {
+    it('all', monad.laws(λ)(Node, run));
+    it('left identity', monad.leftIdentity(λ)(Node, run));
+    it('right identity', monad.rightIdentity(λ)(Node, run));
+    it('associativity', monad.associativity(λ)(Node, run));
   });
 
-  afterEach(function() {
-    // console.log('After "bar" and "qux"');
+  describe('Comonad', function() {
+    it('all', comonad.laws(λ)(Node, run));
+    it('identity', comonad.identity(λ)(Node, run));
+    it('composition', comonad.composition(λ)(Node, run));
+    it('associativity', comonad.associativity(λ)(Node, run));
   });
-
 });
